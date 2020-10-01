a391798748d274ac81b9c5d5bd5e8074df1b709a
Damian Malysza <damian.malysza@gmail.com>
2020-10-01 01:08:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
-def turn_count
-
+def turn_count(board)
+  counter=0
+  board.each do |board|
+    if board == "X" || board == "O"
+      counter+=1
+    end
+  end
+  counter
 end
 
 def current_player
